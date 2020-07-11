540b334fd933a42a44e6cfaf8486baded2c94b63
ignacio <ignacio.chirino@alumnos.ucn.cl>
2020-07-10 21:52:49 -0400

    feat(api.php): We add the API routes.

--- web/routes/api.php
+++ web/routes/api.php
@@ -18,3 +18,8 @@ Route::middleware('auth:api')->get('/user', function (Request $request) {
     return $request->user();
 });
 
+Route::post('/register', 'Api\AuthController@register');
+Route::post('/login', 'Api\AuthController@login');
+
+Route::get('/logout', 'Api\AuthController@logout')->middleware('auth:api');
+Route::apiResource('/personas', 'Api\PersonaController')->middleware('auth:api');
