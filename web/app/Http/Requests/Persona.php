7119b37ec1c5bd7e719fe0cfb46ade3bf1cbe47a
Javier Palacios Arce <Jp.Palacios@Outlook.com>
2020-07-13 17:04:00 -0400

    feat: add of Persona request for validation

---
+++ web/app/Http/Requests/Persona.php
@@ -0,0 +1,34 @@
+<?php
+
+namespace App\Http\Requests;
+
+use Illuminate\Foundation\Http\FormRequest;
+
+class Persona extends FormRequest
+{
+    /**
+     * Determine if the user is authorized to make this request.
+     *
+     * @return bool
+     */
+    public function authorize()
+    {
+        return true;
+    }
+
+    /**
+     * Get the validation rules that apply to the request.
+     *
+     * @return array
+     */
+    public function rules()
+    {
+        return [
+            'rut' => 'requiered|max:50',
+            'name' => 'requiered|max:255',
+            'phone' => 'max:50',
+            'email' => 'requiered|email:rfc,dns|max:255'
+        ];
+    }
+
+}
