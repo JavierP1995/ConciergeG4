a61e566bfb549b3be6f72de5a97228ff872b66b0
ignacio <ignacio.chirino@alumnos.ucn.cl>
2020-07-18 20:58:08 -0400

    build(Http): We create the Visit php model an it's migration.

---
+++ web/database/migrations/2020_07_19_002604_create_visits_table.php
@@ -0,0 +1,34 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateVisitsTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('visits', function (Blueprint $table) {
+            $table->id();
+            $table->string('name');
+            $table->string('rut')->unique();
+            $table->enum('admitted', ['yes', 'no']);
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('visits');
+    }
+}
