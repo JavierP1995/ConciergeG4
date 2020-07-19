3dd4b92a1032f7ae767d9b13037dfac1de84624b
Javier Palacios Arce <Jp.Palacios@Outlook.com>
2020-07-19 00:19:07 -0400

    feat(RecordMigration): foreign key correctly declared

--- web/database/migrations/2020_07_19_024112_create_records_table.php
+++ web/database/migrations/2020_07_19_024112_create_records_table.php
@@ -20,14 +20,17 @@ class CreateRecordsTable extends Migration
             $table->date('entryDate');
             $table->date('departureDate');
             $table->string('comment');
+            $table->biginteger('resident_id');
+            $table->biginteger('visit_id');
             $table->timestamps();
+            $table->foreign('resident_id')->references('id')->on('residents')
+                ->onUpdate('cascade')
+                ->onDelete('cascade');
+            $table->foreign('visit_id')->references('id')->on('visits')
+                ->onUpdate('cascade')
+                ->onDelete('cascade');
 
 
-        });
-
-        Schema::table('records', function (Blueprint $table){
-            $table->foreign('resident_id')->references('id')->on('residents')->onDelete('cascade');
-            $table->foreign('visit_id')->references('id')->on('visits')->onDelete('cascade');
         });
     }
 
