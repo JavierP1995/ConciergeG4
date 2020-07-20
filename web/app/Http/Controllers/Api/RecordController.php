c2d680ccb139414f725a2651199b10170649d5a7
ignacio <ignacio.chirino@alumnos.ucn.cl>
2020-07-20 11:43:17 -0400

    feat(RecordController): Implement the store() method.

--- web/app/Http/Controllers/Api/RecordController.php
+++ web/app/Http/Controllers/Api/RecordController.php
@@ -6,6 +6,7 @@ use App\Http\Controllers\Controller;
 use App\Http\Resources\RecordResource;
 use Illuminate\Http\Request;
 use App\Record;
+use Illuminate\Support\Facades\DB;
 use App\Visit;
 
 class RecordController extends Controller
@@ -32,7 +33,53 @@ class RecordController extends Controller
      */
     public function store(Request $request)
     {
-        //
+        $visit_rut = $request->visit_rut;
+        $visit = DB::table('visits')->where('rut', $visit_rut)->first();
+
+        if($visit!= null){
+
+            $isAdmitted = $visit->admitted;
+            if($isAdmitted == "yes"){
+
+                $department_number = $request->department_number;
+                $department = DB::table('departments')->where('number', $department_number)->first();
+
+                if ($department != null){
+
+                    $department_id = $department->id;
+                    $resident_id =  DB::table('departments')->where('number', $department_number)->first()->resident_id;
+                    $visit_id = $visit->id;
+
+                    $request->request->add(['resident_id' => $resident_id, 'department_id' => $department_id,'visit_id' => $visit_id]);
+
+                    $data = $request->except(['visit_rut', 'department_number']);
+
+
+                    $record = Record::create($data);
+
+                    return response([
+                        'message' => 'Record created successfully',
+                        'record' => new RecordResource($record)
+                    ], 201);
+
+                }else{
+                    return response([
+                        'message' => "The provided number it's not exists !"
+                    ]);
+                }
+
+            }else{
+                return response([
+                    'message' => "The visitant isn't admitted"
+                ]);
+            }
+
+        }else{
+            return response([
+                'message' => "The visitant is not registered !"
+            ]);
+        }
+
     }
 
     /**
