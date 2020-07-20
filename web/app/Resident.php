a6fe4cc0e4fc225a8472a58648ac3be398ef2c8a
gerald-lg <gerald.lopez@alumnos.ucn.cl>
2020-07-20 12:06:32 -0300

    refactor(Model/Resident): define the protected property 'table' for the Residents class

--- web/app/Resident.php
+++ web/app/Resident.php
@@ -9,6 +9,9 @@ class Resident extends Model
    protected $fillable = [
        "rut", "name", "phone", "email"
    ];
+
+   protected $table = 'residents';
+
     public function records(){
         return $this->hasMany('App\Record');
     }
