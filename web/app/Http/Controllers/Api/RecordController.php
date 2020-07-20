5c792e3c1d56147b1239eea0caca45125e5674b7
ignacio <ignacio.chirino@alumnos.ucn.cl>
2020-07-20 14:50:51 -0400

    build(RecordController): Add the Collection dependencies.

--- web/app/Http/Controllers/Api/RecordController.php
+++ web/app/Http/Controllers/Api/RecordController.php
@@ -4,6 +4,7 @@ namespace App\Http\Controllers\Api;
 
 use App\Http\Controllers\Controller;
 use App\Http\Resources\RecordResource;
+use Illuminate\Database\Eloquent\Collection;
 use Illuminate\Http\Request;
 use App\Record;
 use Illuminate\Support\Facades\DB;
