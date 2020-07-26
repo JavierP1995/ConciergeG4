ac15af22cc1925b982ca2b10c15600b3e41575d8
ignacio <ignacio.chirino@alumnos.ucn.cl>
2020-07-25 23:07:39 -0400

    refactor(model): We add the model package and move the Department model into it.

---
+++ Android/app/src/main/java/com/example/android/model/DepartmentModel.kt
@@ -0,0 +1,5 @@
+package com.example.android.model
+
+import com.google.gson.annotations.SerializedName
+
+data class DepartmentModel (@SerializedName("number") var number:Int, @SerializedName("floor") var floor:Int, @SerializedName("block") var block:Char)
--- Android/app/src/main/java/com/example/android/ui/DepartmentResponse.kt
+++
@@ -1,5 +0,0 @@
-package com.example.android.ui
-
-import com.google.gson.annotations.SerializedName
-
-data class DepartmentResponse (@SerializedName("number") var number:Int, @SerializedName("floor") var floor:Int, @SerializedName("block") var block:Char)
