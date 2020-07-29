0a90ab2a7d20c179e55b63bc1e6f44abd3b48a59
ignacio <ignacio.chirino@alumnos.ucn.cl>
2020-07-29 16:17:28 -0400

    feat(DepartmentModel): Change the variables into SerializedName variables.

--- Android/app/src/main/java/com/example/android/model/DepartmentModel.kt
+++ Android/app/src/main/java/com/example/android/model/DepartmentModel.kt
@@ -1,4 +1,6 @@
 package com.example.android.model
 
+import com.google.gson.annotations.SerializedName
 
-data class DepartmentModel(var number:Int, var floor:Int, var block:Char)
+
+data class DepartmentModel(@SerializedName("number") var number:Int, @SerializedName("floor") var floor:Int, @SerializedName("block") var block:Char)
