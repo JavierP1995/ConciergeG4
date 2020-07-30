a8479d43e545c28bc8f432ddf1aa191598c483d5
ignacio <ignacio.chirino@alumnos.ucn.cl>
2020-07-30 10:56:56 -0400

    feat(service): Implement the interfaces with the first GET method.

--- Android/app/src/main/java/com/example/android/service/DepartamentService.kt
+++ Android/app/src/main/java/com/example/android/service/DepartamentService.kt
@@ -1,10 +1,8 @@
 package com.example.android.service
 
 import com.example.android.model.DepartmentModel
-import com.example.android.reponse.DepartmentResponse
 import retrofit2.Call
 import retrofit2.http.GET
-import retrofit2.http.Header
 
 interface DepartmentService {
 
--- Android/app/src/main/java/com/example/android/service/VisitService.kt
+++ Android/app/src/main/java/com/example/android/service/VisitService.kt
@@ -1,2 +1,14 @@
 package com.example.android.service
 
+import com.example.android.model.VisitModel
+import retrofit2.Call
+import retrofit2.http.GET
+
+
+interface VisitService {
+
+    @GET("/api/visits/")
+    fun getVisits(): Call<ArrayList<VisitModel>>
+
+}
+
