a4ee4c5b96b6c7a24caa88a566622f6a6439c615
gerald-lg <gerald.lopez@alumnos.ucn.cl>
2020-07-30 21:09:03 -0400

    refactor(services/DepartmentService): change type of object that receives the call

--- Android/app/src/main/java/com/example/android/service/DepartamentService.kt
+++ Android/app/src/main/java/com/example/android/service/DepartamentService.kt
@@ -9,6 +9,6 @@ import retrofit2.http.Header
 interface DepartmentService {
 
     @GET("/api/departments/")
-    fun getDepartments(): Call<ArrayList<DepartmentModel>>
+    fun getDepartments(): Call<DepartmentResponse>
 
 }
