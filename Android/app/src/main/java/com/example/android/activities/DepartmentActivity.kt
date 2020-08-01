ace2fea6f6e11005424fee1b0c0baf4c9a35266c
gerald-lg <gerald.lopez@alumnos.ucn.cl>
2020-07-31 22:02:52 -0400

    refactor(Activities/DepartmentActivity): updates UI to show departments

--- Android/app/src/main/java/com/example/android/activities/DepartmentActivity.kt
+++ Android/app/src/main/java/com/example/android/activities/DepartmentActivity.kt
@@ -1,6 +1,8 @@
 package com.example.android.activities
 
 import android.content.res.Resources
+import android.graphics.drawable.shapes.OvalShape
+import android.graphics.drawable.shapes.Shape
 import android.os.Bundle
 import androidx.appcompat.app.AppCompatActivity
 import androidx.compose.Composable
@@ -8,25 +10,34 @@ import androidx.compose.getValue
 import androidx.lifecycle.LiveData
 import androidx.lifecycle.MutableLiveData
 import androidx.lifecycle.lifecycleScope
-import androidx.ui.core.Alignment
-import androidx.ui.core.ContextAmbient
-import androidx.ui.core.Modifier
-import androidx.ui.core.setContent
+import androidx.ui.core.*
+import androidx.ui.foundation.Image
 import androidx.ui.foundation.Text
-import androidx.ui.layout.Column
-import androidx.ui.layout.Stack
-import androidx.ui.layout.fillMaxSize
+import androidx.ui.foundation.contentColor
+import androidx.ui.foundation.shape.corner.CircleShape
+import androidx.ui.graphics.Color
+import androidx.ui.graphics.painter.ImagePainter
+import androidx.ui.layout.*
 import androidx.ui.livedata.observeAsState
 import androidx.ui.material.MaterialTheme
 import androidx.ui.material.Scaffold
 import androidx.ui.material.TopAppBar
+import androidx.ui.material.Typography
+import androidx.ui.res.imageResource
+import androidx.ui.text.TextStyle
+import androidx.ui.text.font.FontFamily
 import androidx.ui.text.font.FontStyle
 import androidx.ui.text.style.TextAlign
 import androidx.ui.tooling.preview.Preview
+import androidx.ui.unit.dp
 import androidx.ui.unit.sp
 import com.example.android.ListDepartments
+import com.example.android.R
 import com.example.android.model.DepartmentModel
 import com.example.android.service.Departments
+import com.example.android.ui.typography
+import com.example.android.ui.utils.appTypography
+import com.example.android.ui.utils.darkThemeColors
 import kotlinx.coroutines.Dispatchers
 import kotlinx.coroutines.launch
 import kotlinx.coroutines.withContext
@@ -58,86 +69,98 @@ class DepartmentActivity : AppCompatActivity() {
 
     @Preview
     @Composable
-    private fun topBar() {
-        TopAppBar {
-            Text(
-                text = "List of Departments.",
-                modifier = Modifier.weight(1f),
-                fontStyle = FontStyle.Normal,
-                fontSize = 20.sp,
-                textAlign = TextAlign.Center
-            )
-        }
+    private fun topBar() =
+            MaterialTheme( colors = darkThemeColors) {
 
-    }
+
+                TopAppBar(
+                        title =
+                        { Text(
+                                text = "Departments",
+                                style = MaterialTheme.typography.h5
+                                )
+                        },
+                        backgroundColor = darkThemeColors.primary,
+                        elevation = 0.dp
+                )
+            }
 
     @Composable
     private fun listDepartments(departments : LiveData<ListDepartments>){
 
         val deptos by departments.observeAsState()
-
-        Scaffold(
-            topAppBar = {
-                topBar()
-            },
-            bodyContent = {
-                if(deptos == null || deptos?.loading == true){
-                    Loading()
-                }else{
-                    Column() {
-                        deptos!!.departments.let {
-                            it.forEach { department ->
-                                    printDepartment(department = department)
+        MaterialTheme(colors = darkThemeColors) {
+            Scaffold(
+                    topAppBar = {
+                        topBar()
+                    },
+                    bodyContent = {
+                        if(deptos == null || deptos?.loading == true){
+                            Loading()
+                        }else{
+                            Column() {
+                                deptos!!.departments.let {
+                                    it.forEach { department ->
+                                        printDepartment(department = department)
+                                    }
                                 }
+                            }
                         }
                     }
-                }
-            }
-        )
-
-    }
-    @Composable
-    fun Loading() {
-        Stack(modifier = Modifier.fillMaxSize()) {
-            Text(
-                text = "Nada aquí",
-                style = MaterialTheme.typography.h6,
-                modifier = Modifier.gravity(Alignment.Center)
             )
         }
 
     }
     @Composable
-    private fun printDepartment(department: DepartmentModel) {
-        Column {
-            Text(text = "Number: "+department.number.toString())
-            Text(text = "Floor: "+department.floor.toString())
-            Text(text = "Block: "+department.block.toString())
+    fun Loading() {
+        MaterialTheme(colors = darkThemeColors) {
+            Stack(modifier = Modifier.fillMaxSize()) {
+                Text(
+                        text = "Loading",
+                        style = MaterialTheme.typography.h6,
+                        modifier = Modifier.gravity(Alignment.Center)
+                )
+            }
+
         }
     }
+    @Composable
+    private fun printDepartment(department: DepartmentModel) {
+        Column (
+                modifier = Modifier.padding(0.dp, 10.dp)
+        ){
+            Row{
 
-    /**private fun callDepartments(): ArrayList<DepartmentModel> {
+                Image(
+                        painter = ImagePainter(imageResource(id = R.drawable.home)),
+                        modifier = Modifier.preferredSize(35.dp).clip(shape = CircleShape),
+                        alignment = Alignment.TopStart
+                )
 
-    val departmentService = ApiService.buildService(DepartmentService::class.java)
+                Text(
+                        text = department.number.toString(),
+                        color = darkThemeColors.onPrimary,
+                        modifier = Modifier.padding(10.dp, 5.dp).size(40.dp)
 
-    val requestCall: Call<ArrayList<DepartmentModel>> = departmentService.getDepartments();
+                )
+            }
+            Row(
+            ){
+                Text(
+                        text = "Floor: "+department.floor.toString(),
+                        style = MaterialTheme.typography.subtitle2,
+                        color = darkThemeColors.onPrimary
 
-    val response = requestCall.execute()
+                    )
 
-    if(!response.isSuccessful){
-    // Your status code is in the range of 300's, 400's and 500's
-    Toast.makeText(
-    this@DepartmentActivity,
-    "Failed to retrieve items",
-    Toast.LENGTH_LONG
-    ).show()
+                Text(
+                        text = "Block: "+department.block.toString(),
+                        style = MaterialTheme.typography.subtitle2,
+                        color = darkThemeColors.onPrimary,
+                        modifier = Modifier.padding(20.dp,0.dp)
+
+                        )
+            }
+        }
     }
-    else if( response.code() == 401) {
-    Toast.makeText(
-    this@DepartmentActivity,
-    "Your session has expired. Please Login again.", Toast.LENGTH_LONG
-    ).show()
-    }
-    return response.body()!!
-    }*/
 }
