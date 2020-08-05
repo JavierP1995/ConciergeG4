dca2f10be01b7226e2b4e02c12b8de2fc1651bf0
Javier Palacios Arce <Jp.Palacios@Outlook.com>
2020-08-05 00:24:02 -0400

    feat(ResidentAdapter): function to create added, new route to api added

--- Android/app/src/main/java/com/example/android/adapter/ResidentAdapter.kt
+++ Android/app/src/main/java/com/example/android/adapter/ResidentAdapter.kt
@@ -21,4 +21,19 @@ object ResidentAdapter {
         }
         return null
     }
+
+    fun registerResident(rut: String, name: String, email: String, phone: Int, department: Int): ResidentModel? {
+        val call: Call<ResidentModel> = ApiService
+                .buildService(ResidentService::class.java)
+                .createResident(rut = rut, name = name, email = email, phone = phone,  department_id = department)
+        try{
+            val response = call.execute()
+            Log.v("Json", response.body()!!.toString())
+            return ResidentModel(rut = rut, name = name, email = email, phone = phone, department_id = department)
+        } catch (e: Exception){
+            e.printStackTrace()
+        }
+        return null
+    }
+
 }
