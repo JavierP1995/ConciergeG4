fefeaf31161dd1ea530a831efebf7e13c3ab9bf0
gerald-lg <gerald.lopez@alumnos.ucn.cl>
2020-08-06 21:08:46 -0400

    feat(Response): delete deprecate class

--- Android/app/src/main/java/com/example/android/reponse/LoginResponse.kt
+++
@@ -1,5 +0,0 @@
-package com.example.android.reponse
-
-import com.example.android.model.UserModel
-
-data class LoginResponse(val message: String, val user: UserModel, val token: String, val tokenExpiresAt: String)
