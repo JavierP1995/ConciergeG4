ec86658b553125a21812cb64ebee397e1ddc685e
gerald-lg <gerald.lopez@alumnos.ucn.cl>
2020-08-06 20:51:28 -0400

    feat(Adapter/AuthAdapter): add funtions to send data to a server to create user and login

---
+++ Android/app/src/main/java/com/example/android/adapter/AuthAdapter.kt
@@ -0,0 +1,42 @@
+package com.example.android.adapter
+
+import android.util.Log
+import com.example.android.model.UserModel
+import com.example.android.reponse.LoginResponse
+import com.example.android.reponse.RegisterResponse
+import com.example.android.service.ApiService
+import com.example.android.service.AuthService
+import retrofit2.Call
+import java.lang.Exception
+
+object AuthAdapter {
+
+    fun registerUser(name : String, email: String, password : String, password_confirmation: String){
+
+        val requestCall : Call<RegisterResponse> = ApiService.
+        buildService(AuthService::class.java).register(name, email, password, password_confirmation)
+        try {
+            val response = requestCall.execute()
+            Log.v("Json", response.body().toString())
+
+        }catch (e: Exception){
+            e.printStackTrace()
+        }
+
+    }
+
+    fun loginUser(email : String, password: String){
+
+        val requestCall : Call<LoginResponse> = ApiService.
+        buildService(AuthService::class.java).login(email, password)
+
+        try{
+            val response  = requestCall.execute()
+            Log.v("Json", response.body().toString())
+
+        }catch (e: Exception){
+            e.printStackTrace()
+        }
+    }
+
+}
