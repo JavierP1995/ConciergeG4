0c02caf040dfaa769788ee4c4ef9af81509443df
Javier Palacios Arce <Jp.Palacios@Outlook.com>
2020-08-09 03:03:38 -0400

    feat(modelServices): new routes for search added

--- Android/app/src/main/java/com/example/android/service/DepartmentService.kt
+++ Android/app/src/main/java/com/example/android/service/DepartmentService.kt
@@ -4,11 +4,21 @@ import com.example.android.model.DepartmentModel
 import retrofit2.Call
 import retrofit2.http.GET
 import retrofit2.http.POST
+import retrofit2.http.Path
 import retrofit2.http.Query
 
 interface DepartmentService {
     @GET("departments/")
     fun getDepartments():Call<ArrayList<DepartmentModel>>
+
+    @GET("departments/{search}/")
+    fun searchByNumber(@Path("search") number: String) : Call<ArrayList<DepartmentModel>>
+
+    @GET("departments/{search}/resident/")
+    fun searchByResident(@Path("search") rut: String): Call<ArrayList<DepartmentModel>>
+
+    @GET("departments/{search}/visit/")
+    fun searchByVisit(@Path("search") rut: String): Call<ArrayList<DepartmentModel>>
 
     @POST("/api/departments/")
     fun createDepartment(@Query("number") number: Int, @Query("floor") floor: Int? ,@Query("block") block: Char?): Call<DepartmentModel>
