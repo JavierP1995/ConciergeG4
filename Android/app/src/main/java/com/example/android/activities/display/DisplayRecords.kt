2c023cebae6c842935ab39acf44ef30edf361909
gerald-lg <gerald.lopez@alumnos.ucn.cl>
2020-08-15 18:47:05 -0400

    refactor(display): modify name of class

--- Android/app/src/main/java/com/example/android/activities/display/DisplayRecords.kt
+++ Android/app/src/main/java/com/example/android/activities/display/DisplayRecords.kt
@@ -32,7 +32,7 @@ import kotlinx.coroutines.Dispatchers
 import kotlinx.coroutines.launch
 import kotlinx.coroutines.withContext
 
-class displayRecords : AppCompatActivity() {
+class DisplayRecords : AppCompatActivity() {
 
 
     private val recordsList = MutableLiveData<ListRecords>().apply {
