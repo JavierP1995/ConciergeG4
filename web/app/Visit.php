389f2f094f974528a7f2b29058a565b3cc0453c9
ignacio <ignacio.chirino@alumnos.ucn.cl>
2020-08-18 10:28:30 -0400

    refactor(Visit.php): remove the id field on the fillable array.

--- web/app/Visit.php
+++ web/app/Visit.php
@@ -7,7 +7,7 @@ use Illuminate\Database\Eloquent\Model;
 class Visit extends Model
 {
     protected $fillable = [
-        'id', 'name', 'rut', 'admitted'
+        'name', 'rut', 'admitted'
     ];
     protected $table = 'visits';
 
