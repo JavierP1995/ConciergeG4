3deb807724a144b24a52c7d5e5dafcfc146755d6
Javier Palacios Arce <Jp.Palacios@Outlook.com>
2020-08-18 01:32:59 -0400

    fix(DepartmentService): missing headers added

--- Android/app/src/main/java/com/example/android/service/DepartmentService.kt
+++ Android/app/src/main/java/com/example/android/service/DepartmentService.kt
@@ -10,18 +10,22 @@ interface DepartmentService {
     fun getDepartments(@Header("Authorization") auth:String):Call<ArrayList<DepartmentModel>>
 
     @GET("departments/{search}/")
+    @Headers("Accept: application/json")
     fun searchByNumber(@Header("Authorization") auth:String,
                        @Path("search") number: String) : Call<ArrayList<DepartmentModel>>
 
     @GET("departments/{search}/resident/")
+    @Headers("Accept: application/json")
     fun searchByResident(@Header("Authorization") auth:String,
                          @Path("search") rut: String): Call<ArrayList<DepartmentModel>>
 
     @GET("departments/{search}/visit/")
+    @Headers("Accept: application/json")
     fun searchByVisit(@Header("Authorization") auth:String,
                       @Path("search") rut: String): Call<ArrayList<DepartmentModel>>
 
     @POST("/api/departments/")
+    @Headers("Accept: application/json")
     fun createDepartment(@Header("Authorization") auth:String,
                          @Query("number") number: Int,
                          @Query("floor") floor: Int?,
