f3e7f474396da94f86bda7f95baccdf55382b180
gerald-lg <gerald.lopez@alumnos.ucn.cl>
2020-08-17 23:24:04 -0400

    refactor(services): modify parameteres of the methods to send token

--- Android/app/src/main/java/com/example/android/service/VisitService.kt
+++ Android/app/src/main/java/com/example/android/service/VisitService.kt
@@ -5,22 +5,26 @@ import com.example.android.model.ResidentModel
 import com.example.android.model.VisitModel
 import retrofit2.Call
 import retrofit2.http.GET
+import retrofit2.http.Header
 import retrofit2.http.Path
 
 
 interface VisitService {
 
     @GET("visits")
-    fun getVisits(): Call<ArrayList<VisitModel>>
+    fun getVisits(@Header("Authorization") auth:String): Call<ArrayList<VisitModel>>
 
     @GET("visits/{search}")
-    fun searchByRut(@Path("search") rut: String): Call<ArrayList<VisitModel>>
+    fun searchByRut(@Header("Authorization") auth:String,
+                    @Path("search") rut: String): Call<ArrayList<VisitModel>>
 
     @GET("visits/{search}/resident")
-    fun searchByResident(@Path("search") rut: String): Call<ArrayList<VisitModel>>
+    fun searchByResident(@Header("Authorization") auth:String,
+                         @Path("search") rut: String): Call<ArrayList<VisitModel>>
 
     @GET("visits/{search}/department")
-    fun searchByDepartment(@Path("search") number: String): Call<ArrayList<VisitModel>>
+    fun searchByDepartment(@Header("Authorization") auth:String,
+                           @Path("search") number: String): Call<ArrayList<VisitModel>>
 
 }
 
