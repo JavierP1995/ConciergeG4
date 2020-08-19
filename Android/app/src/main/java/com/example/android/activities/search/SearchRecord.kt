acc9b2b09c09db41cefde3faec9adb17c4df311f
gerald-lg <gerald.lopez@alumnos.ucn.cl>
2020-08-19 00:54:13 -0400

    refactor(activities/search): update UI of search activities

--- Android/app/src/main/java/com/example/android/activities/search/SearchRecord.kt
+++ Android/app/src/main/java/com/example/android/activities/search/SearchRecord.kt
@@ -8,137 +8,173 @@ import androidx.compose.Composable
 import androidx.compose.state
 import androidx.ui.core.Alignment
 import androidx.ui.core.Modifier
+import androidx.ui.core.paint
 import androidx.ui.core.setContent
 import androidx.ui.foundation.*
+import androidx.ui.foundation.shape.corner.RoundedCornerShape
+import androidx.ui.graphics.painter.ImagePainter
+import androidx.ui.input.KeyboardType
 import androidx.ui.layout.*
 import androidx.ui.material.*
+import androidx.ui.res.imageResource
 import androidx.ui.tooling.preview.Preview
 import androidx.ui.unit.dp
+import com.example.android.R
 import com.example.android.activities.display.DisplayRecords
 import com.example.android.ui.utils.darkThemeColors
 
+/**
+ * Activity to use search record by number, rut of resident or visit
+ */
 class SearchRecord : AppCompatActivity() {
 
-    override fun onCreate(savedInstanceState: Bundle?) {
-        super.onCreate(savedInstanceState)
-        setContent {
-            MaterialTheme {
-                topBar()
-            }
-        }
-    }
-
     companion object{
+        /**
+         * Global variable used to receive and send the token in the methods.
+         */
         var token : String = ""
 
+        /**
+         * Method used to change the token value
+         */
         fun setLoginData(authToken : String){
             this.token = authToken
             Log.v("TOKEN", this.token)
         }
     }
 
-    @Preview
-    @Composable
-    private fun topBar() =
-            MaterialTheme( colors = darkThemeColors) {
-                TopAppBar(
-                        title =
-                        {
-                            Text(text = "Records Search",
-                                    style = MaterialTheme.typography.h5)
-                        },
-                        backgroundColor = darkThemeColors.primary,
-                        elevation = 0.dp
-                )
+    /**
+     * Method used to start the activity.
+     */
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContent {
+            MaterialTheme {
                 searchMenu()
             }
+        }
+    }
 
+    /**
+     * Method used display menu with options to search records
+     */
+    @Preview
     @Composable
     private fun searchMenu() {
         val searchD = state { TextFieldValue("") }
         val searchR = state { TextFieldValue("") }
         val searchV = state { TextFieldValue("") }
-        Column(
-                modifier = Modifier.fillMaxSize(),
-                verticalArrangement = Arrangement.Center,
-                horizontalGravity = Alignment.CenterHorizontally
-        ) {
-            Row(
-                    horizontalArrangement = Arrangement.Center,
-                    verticalGravity = Alignment.CenterVertically
-            ) {
-                FilledTextField(
-                        value = searchD.value,
-                        activeColor = darkColorPalette().secondary,
-                        onValueChange = { searchD.value = it },
-                        label = { Text("Number: ") }
-                )
-                Spacer(modifier = Modifier.padding(bottom = 20.dp))
-                Button(
-                        text = { Text("Search By Department") },
-                        backgroundColor = darkColorPalette().secondary,
-                        modifier = Modifier.padding(0.dp, 10.dp),
-                        onClick = {
-                            val intent = Intent(this@SearchRecord,
-                                    DisplayRecords::class.java)
-                            intent.putExtra("option", "byDepartment")
-                            intent.putExtra("search", searchD.value.text)
-                            startActivity(intent)
+
+        MaterialTheme(colors= darkThemeColors) {
+            Scaffold(
+                    bodyContent = {
+
+                        Image(
+                                painter = ImagePainter(imageResource(id = R.drawable.appbarsearch)),
+                                alignment = Alignment.TopCenter
+                        )
+                        Image(
+                                painter = ImagePainter(imageResource(id = R.drawable.conciergewallpaper)),
+                                alignment = Alignment.Center,
+                                modifier = Modifier.fillMaxSize()
+                        )
+
+                        Column(
+                                modifier = Modifier.fillMaxSize(),
+                                verticalArrangement = Arrangement.Center,
+                                horizontalGravity = Alignment.CenterHorizontally
+                        ) {
+                            Row(
+                                    horizontalArrangement = Arrangement.Center,
+                                    verticalGravity = Alignment.CenterVertically
+                            ) {
+                                FilledTextField(
+                                        value = searchD.value,
+                                        shape = RoundedCornerShape(10.dp),
+                                        activeColor = darkColorPalette().secondary,
+                                        onValueChange = { searchD.value = it },
+                                        label = { Text("Search by number: ") },
+                                        keyboardType = KeyboardType.Number
+                                )
+                                Spacer(modifier = Modifier.padding(bottom = 20.dp))
+                                Button(
+                                        text = { Text("Search By Department") },
+                                        backgroundColor = darkColorPalette().secondary,
+                                        modifier = Modifier.paint(painter = ImagePainter(imageResource(id = R.drawable.searchicon)),
+                                                sizeToIntrinsics = false),
+                                        onClick = {
+                                            val intent = Intent(this@SearchRecord,
+                                                    DisplayRecords::class.java)
+                                            intent.putExtra("option", "byDepartment")
+                                            intent.putExtra("search", searchD.value.text)
+                                            startActivity(intent)
+                                        }
+                                )
+                            }
+                            Spacer(modifier = Modifier.padding(top = 20.dp))
+                            Row(
+                                    horizontalArrangement = Arrangement.Center,
+                                    verticalGravity = Alignment.CenterVertically
+                            ) {
+                                FilledTextField(
+                                        value = searchR.value,
+                                        shape = RoundedCornerShape(10.dp),
+                                        activeColor = darkColorPalette().secondary,
+                                        onValueChange = { searchR.value = it },
+                                        label = { Text("Search by rut of Resident ") }
+                                )
+                                Spacer(modifier = Modifier.padding(bottom = 20.dp))
+                                Button(
+                                        text = { Text("Search By Resident") },
+                                        backgroundColor = darkColorPalette().secondary,
+                                        modifier = Modifier.paint(painter = ImagePainter(imageResource(id = R.drawable.searchicon)),
+                                                sizeToIntrinsics = false),
+                                        onClick = {
+                                            val intent = Intent(this@SearchRecord,
+                                                    DisplayRecords::class.java)
+                                            intent.putExtra("Authorization", SearchRecord.token)
+                                            intent.putExtra("option", "byResident")
+                                            intent.putExtra("search", searchR.value.text)
+                                            startActivity(intent)
+                                        }
+                                )
+                            }
+                            Spacer(modifier = Modifier.padding(top = 20.dp))
+                            Row(
+                                    horizontalArrangement = Arrangement.Center,
+                                    verticalGravity = Alignment.CenterVertically
+                            ) {
+                                FilledTextField(
+                                        value = searchV.value,
+                                        shape = RoundedCornerShape(10.dp),
+                                        activeColor = darkColorPalette().secondary,
+                                        onValueChange = { searchV.value = it },
+                                        label = { Text("Search by rut of visit: ") }
+                                )
+                                Spacer(modifier = Modifier.padding(bottom = 20.dp))
+                                Button(
+                                        text = { Text("Search By Visit") },
+                                        backgroundColor = darkColorPalette().secondary,
+                                        modifier = Modifier.paint(painter = ImagePainter(imageResource(id = R.drawable.searchicon)),
+                                                sizeToIntrinsics = false),
+                                        onClick = {
+                                            val intent = Intent(this@SearchRecord,
+                                                    DisplayRecords::class.java)
+                                            intent.putExtra("option", "byVisit")
+                                            intent.putExtra("search", searchV.value.text)
+                                            startActivity(intent)
+                                        }
+                                )
+                            }
                         }
-                )
-            }
-            Spacer(modifier = Modifier.padding(top = 20.dp))
-            Row(
-                    horizontalArrangement = Arrangement.Center,
-                    verticalGravity = Alignment.CenterVertically
-            ) {
-                FilledTextField(
-                        value = searchR.value,
-                        activeColor = darkColorPalette().secondary,
-                        onValueChange = { searchR.value = it },
-                        label = { Text("Rut: ") }
-                )
-                Spacer(modifier = Modifier.padding(bottom = 20.dp))
-                Button(
-                        text = { Text("Search By Resident") },
-                        backgroundColor = darkColorPalette().secondary,
-                        modifier = Modifier.padding(0.dp, 10.dp),
-                        onClick = {
-                            val intent = Intent(this@SearchRecord,
-                                    DisplayRecords::class.java)
-                            intent.putExtra("Authorization", SearchRecord.token)
-                            intent.putExtra("option", "byResident")
-                            intent.putExtra("search", searchR.value.text)
-                            startActivity(intent)
-                        }
-                )
-            }
-            Spacer(modifier = Modifier.padding(top = 20.dp))
-            Row(
-                    horizontalArrangement = Arrangement.Center,
-                    verticalGravity = Alignment.CenterVertically
-            ) {
-                FilledTextField(
-                        value = searchV.value,
-                        activeColor = darkColorPalette().secondary,
-                        onValueChange = { searchV.value = it },
-                        label = { Text("Rut: ") }
-                )
-                Spacer(modifier = Modifier.padding(bottom = 20.dp))
-                Button(
-                        text = { Text("Search By Visit") },
-                        backgroundColor = darkColorPalette().secondary,
-                        modifier = Modifier.padding(0.dp, 10.dp),
-                        onClick = {
-                            val intent = Intent(this@SearchRecord,
-                                    DisplayRecords::class.java)
-                            intent.putExtra("option", "byVisit")
-                            intent.putExtra("search", searchV.value.text)
-                            startActivity(intent)
-                        }
-                )
-            }
+                    })
         }
+
+
     }
 
+
 }
+
+
+
--- Android/app/src/main/java/com/example/android/activities/search/SearchVisit.kt
+++ Android/app/src/main/java/com/example/android/activities/search/SearchVisit.kt
@@ -8,137 +8,172 @@ import androidx.compose.Composable
 import androidx.compose.state
 import androidx.ui.core.Alignment
 import androidx.ui.core.Modifier
+import androidx.ui.core.paint
 import androidx.ui.core.setContent
 import androidx.ui.foundation.*
+import androidx.ui.foundation.shape.corner.RoundedCornerShape
+import androidx.ui.graphics.painter.ImagePainter
 import androidx.ui.layout.*
 import androidx.ui.material.*
+import androidx.ui.res.imageResource
 import androidx.ui.tooling.preview.Preview
 import androidx.ui.unit.dp
+import com.example.android.R
 import com.example.android.activities.display.DisplayVisits
 import com.example.android.ui.utils.darkThemeColors
 
+/**
+ * Activity to use search visit by number of department, rut of resident or visit
+ */
 class SearchVisit : AppCompatActivity() {
 
-    override fun onCreate(savedInstanceState: Bundle?) {
-        super.onCreate(savedInstanceState)
-        setContent {
-            MaterialTheme {
-                topBar()
-            }
-        }
-    }
-
     companion object{
+        /**
+         * Global variable used to receive and send the token in the methods.
+         */
         var token : String = ""
 
+        /**
+         * Method used to change the token value
+         */
         fun setLoginData(authToken : String){
             this.token = authToken
             Log.v("TOKEN", this.token)
         }
     }
 
-    @Preview
-    @Composable
-    private fun topBar() =
-            MaterialTheme( colors = darkThemeColors) {
-                TopAppBar(
-                        title =
-                        {
-                            Text(text = "Visit Search",
-                                    style = MaterialTheme.typography.h5)
-                        },
-                        backgroundColor = darkThemeColors.primary,
-                        elevation = 0.dp
-                )
+    /**
+     * Method used to start the activity.
+     */
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContent {
+            MaterialTheme {
                 searchMenu()
             }
+        }
+    }
 
+    /**
+     * Method used to display menu with options to search visit
+     */
+    @Preview
     @Composable
     private fun searchMenu() {
+        val searchV = state { TextFieldValue("") }
         val searchR = state { TextFieldValue("") }
-        val searchRe = state { TextFieldValue("") }
         val searchD = state { TextFieldValue("") }
-        Column(
-                modifier = Modifier.fillMaxSize(),
-                verticalArrangement = Arrangement.Center,
-                horizontalGravity = Alignment.CenterHorizontally
-        ) {
-            Row(
-                    horizontalArrangement = Arrangement.Center,
-                    verticalGravity = Alignment.CenterVertically
-            ) {
-                FilledTextField(
-                        value = searchR.value,
-                        activeColor = darkColorPalette().secondary,
-                        onValueChange = { searchR.value = it },
-                        label = { Text("Rut: ") }
-                )
-                Spacer(modifier = Modifier.padding(bottom = 20.dp))
-                Button(
-                        text = { Text("Search By Rut") },
-                        backgroundColor = darkColorPalette().secondary,
-                        modifier = Modifier.padding(0.dp, 10.dp),
-                        onClick = {
-                            val intent = Intent(this@SearchVisit,
-                                    DisplayVisits::class.java)
-                            intent.putExtra("option", "byRut")
-                            intent.putExtra("search", searchR.value.text)
-                            startActivity(intent)
+
+        MaterialTheme(colors = darkThemeColors) {
+
+            Scaffold(
+                    bodyContent = {
+
+                        Image(
+                                painter = ImagePainter(imageResource(id = R.drawable.appbarsearch)),
+                                alignment = Alignment.TopCenter
+                        )
+                        Image(
+                                painter = ImagePainter(imageResource(id = R.drawable.conciergewallpaper)),
+                                alignment = Alignment.Center,
+                                modifier = Modifier.fillMaxSize()
+                        )
+
+                        Column(
+                                modifier = Modifier.fillMaxSize(),
+                                verticalArrangement = Arrangement.Center,
+                                horizontalGravity = Alignment.CenterHorizontally
+                        ) {
+
+                            Row(
+                                    horizontalArrangement = Arrangement.Center,
+                                    verticalGravity = Alignment.CenterVertically
+                            ) {
+                                FilledTextField(
+                                        value = searchD.value,
+                                        activeColor = darkColorPalette().secondary,
+                                        shape = RoundedCornerShape(10.dp),
+                                        onValueChange = { searchD.value = it },
+                                        label = { Text("Search by number of department ") }
+                                )
+                                Spacer(modifier = Modifier.padding(bottom = 20.dp))
+                                Button(
+                                        text = { Text("Search By Department") },
+                                        backgroundColor = darkColorPalette().secondary,
+                                        modifier = Modifier.paint(painter = ImagePainter(imageResource(id = R.drawable.searchicon)),
+                                                sizeToIntrinsics = false),
+                                        onClick = {
+                                            val intent = Intent(this@SearchVisit,
+                                                    DisplayVisits::class.java)
+                                            intent.putExtra("Authorization", SearchVisit.token)
+                                            intent.putExtra("option", "byDepartment")
+                                            intent.putExtra("search", searchD.value.text)
+                                            startActivity(intent)
+                                        }
+                                )
+                            }
+                            Spacer(modifier = Modifier.padding(top = 20.dp))
+                            Row(
+                                    horizontalArrangement = Arrangement.Center,
+                                    verticalGravity = Alignment.CenterVertically
+                            ) {
+                                FilledTextField(
+                                        value = searchV.value,
+                                        activeColor = darkColorPalette().secondary,
+                                        shape = RoundedCornerShape(10.dp),
+                                        onValueChange = { searchV.value = it },
+                                        label = { Text("Search by rut of visit ") }
+                                )
+                                Spacer(modifier = Modifier.padding(bottom = 20.dp))
+                                Button(
+                                        text = { Text("Search By Rut") },
+                                        backgroundColor = darkColorPalette().secondary,
+                                        modifier = Modifier.paint(painter = ImagePainter(imageResource(id = R.drawable.searchicon)),
+                                                sizeToIntrinsics = false),
+                                        onClick = {
+                                            val intent = Intent(this@SearchVisit,
+                                                    DisplayVisits::class.java)
+                                            intent.putExtra("option", "byRut")
+                                            intent.putExtra("search", searchV.value.text)
+                                            startActivity(intent)
+                                        }
+                                )
+                            }
+                            Spacer(modifier = Modifier.padding(top = 20.dp))
+                            Row(
+                                    horizontalArrangement = Arrangement.Center,
+                                    verticalGravity = Alignment.CenterVertically
+                            ) {
+                                FilledTextField(
+                                        value = searchR.value,
+                                        activeColor = darkColorPalette().secondary,
+                                        shape = RoundedCornerShape(10.dp),
+                                        onValueChange = { searchR.value = it },
+                                        label = { Text("Search by rut of resident ") }
+                                )
+                                Spacer(modifier = Modifier.padding(bottom = 20.dp))
+                                Button(
+                                        text = { Text("Search by rut of resident") },
+                                        backgroundColor = darkColorPalette().secondary,
+                                        modifier = Modifier.paint(painter = ImagePainter(imageResource(id = R.drawable.searchicon)),
+                                                sizeToIntrinsics = false),
+                                        onClick = {
+                                            val intent = Intent(this@SearchVisit,
+                                                    DisplayVisits::class.java)
+                                            intent.putExtra("option", "byResident")
+                                            intent.putExtra("search", searchR.value.text)
+                                            startActivity(intent)
+                                        }
+                                )
+                            }
                         }
-                )
-            }
-            Spacer(modifier = Modifier.padding(top = 20.dp))
-            Row(
-                    horizontalArrangement = Arrangement.Center,
-                    verticalGravity = Alignment.CenterVertically
-            ) {
-                FilledTextField(
-                        value = searchRe.value,
-                        activeColor = darkColorPalette().secondary,
-                        onValueChange = { searchRe.value = it },
-                        label = { Text("Rut: ") }
-                )
-                Spacer(modifier = Modifier.padding(bottom = 20.dp))
-                Button(
-                        text = { Text("Search By Resident") },
-                        backgroundColor = darkColorPalette().secondary,
-                        modifier = Modifier.padding(0.dp, 10.dp),
-                        onClick = {
-                            val intent = Intent(this@SearchVisit,
-                                    DisplayVisits::class.java)
-                            intent.putExtra("option", "byResident")
-                            intent.putExtra("search", searchRe.value.text)
-                            startActivity(intent)
-                        }
-                )
-            }
-            Spacer(modifier = Modifier.padding(top = 20.dp))
-            Row(
-                    horizontalArrangement = Arrangement.Center,
-                    verticalGravity = Alignment.CenterVertically
-            ) {
-                FilledTextField(
-                        value = searchD.value,
-                        activeColor = darkColorPalette().secondary,
-                        onValueChange = { searchD.value = it },
-                        label = { Text("Rut: ") }
-                )
-                Spacer(modifier = Modifier.padding(bottom = 20.dp))
-                Button(
-                        text = { Text("Search By Department") },
-                        backgroundColor = darkColorPalette().secondary,
-                        modifier = Modifier.padding(0.dp, 10.dp),
-                        onClick = {
-                            val intent = Intent(this@SearchVisit,
-                                    DisplayVisits::class.java)
-                            intent.putExtra("Authorization", SearchVisit.token)
-                            intent.putExtra("option", "byDepartment")
-                            intent.putExtra("search", searchD.value.text)
-                            startActivity(intent)
-                        }
-                )
-            }
+
+            })
+
         }
+
+
+
     }
 
 }
