3872cef37a5275ec74f5cb57b00949e96105ec42
ignacio <ignacio.chirino@alumnos.ucn.cl>
2020-08-20 20:47:42 -0400

    feat(api.php): add logout route.

--- web/routes/api.php
+++ web/routes/api.php
@@ -20,6 +20,7 @@ Route::middleware('auth:api')->get('/user', function (Request $request) {
 
 Route::post('/register', 'Api\AuthController@register');
 Route::post('/login', 'Api\AuthController@login');
+Route::post('/logout', 'Api\AuthController@logout');
 
 Route::get('/logout', 'Api\AuthController@logout')->middleware('auth:api');
 
